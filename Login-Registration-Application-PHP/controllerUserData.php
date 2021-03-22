18de8c8bd98744ef82e50fbcfee7794e48db2900
Michael O'Grady <mogrady.professional@gmail.com>
2021-03-22 18:32:00 +0000

    Project Upgrade

--- Login-Registration-Application-PHP/controllerUserData.php
+++ Login-Registration-Application-PHP/controllerUserData.php
@@ -29,7 +29,6 @@ if (isset($_POST['signup'])) {
         if ($data_check) {
             $subject = "Email Verification Code";
             $message = "Your verification code is $code";
-            // $sender = "From: shahiprem7890@gmail.com";
             $sender = "From: mogrady.professional@gmail.com";
             if (mail($email, $subject, $message, $sender)) {
                 $info = "We've sent a verification code to your email - $email";
